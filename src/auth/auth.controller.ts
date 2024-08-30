7b271b7f8d52cea90375e3ab86bf715b81e6bfb6
jasmin.jani <jasmin.jani@esparkbizmail.com>
2024-08-30 12:54:04 +0530

    cookie-parser added, apply cookieParser middleware in main.ts and sending response from controller for signIn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,38 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post()
-  userSignIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.userSignIn(signInDto.userName, signInDto.password);
+  async userSignIn(
+    @Body() signInDto: Record<string, any>,
+    @Res() res: Response,
+  ) {
+    const user = await this.authService.validateUser(
+      signInDto.userName,
+      signInDto.password,
+    );
+
+    if (!user) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: 'Invalid credentials',
+      });
+    }
+
+    const jwt_token = await this.authService.generateJwtToken(user);
+    user.jwt_token = jwt_token;
+
+    return res
+      .cookie('jwt_token', jwt_token, {
+        httpOnly: true,
+        maxAge: 3600000,
+      })
+      .status(HttpStatus.CREATED)
+      .json({
+        user: user,
+      });
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import {
-  BadRequestException,
+  // BadRequestException,
   Injectable,
-  UnauthorizedException,
+  // UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { DatabaseService } from 'src/database/database.service';
@@ -26,24 +26,29 @@ export class AuthService {
   //   },
   // ];
 
-  async userSignIn(username: string, pass: string) {
-    // console.log('username :>> ', username);
+  async validateUser(username: string, pass: string) {
     const user = await this.findUser(username);
-    // console.log('user :>> ', user);
 
-    if (user?.length < 1) {
-      throw new BadRequestException();
-    }
+    // if (user?.length < 1) {
+    //   throw new BadRequestException('Invalide credential');
+    // }
 
-    if (user && user[0]?.password === pass) {
-      const payload = { sub: user[0].userId, username: user[0].userName };
-      // return { access_token: await this.jwtService.signAsync(payload) };
-      const jwt_token = await this.jwtService.signAsync(payload);
-
-      return { message: 'User Login Successfully', jwt_token };
+    if (user?.length && user[0]?.password === pass) {
+      // const payload = { sub: user[0].userId, username: user[0].userName };
+      // const jwt_token = await this.jwtService.signAsync(payload);
+      // return { message: 'User Login Successfully', jwt_token };
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user[0];
+      return result;
     } else {
-      throw new UnauthorizedException();
+      // throw new UnauthorizedException();
     }
+    return null;
+  }
+
+  async generateJwtToken(user: any) {
+    const payload = { sub: user.userId, username: user.userName };
+    return await this.jwtService.signAsync(payload);
   }
 
   async findUser(username: string) {
